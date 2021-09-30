4dbdd017edbd4f189058e31bcf8ff752e02f39f5
pzcn <sdustpedro@gmail.com>
2021-09-30 14:54:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5487,33 +5487,33 @@ if (UI.dark_mode) {
 }
 if (UI.dark_mode) {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/css/mdui_v2_dark.min.css">'
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/cheems/goindex-extended/css/mdui_v2_dark.min.css">'
   );
 } else {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/css/mdui_v2.min.css">'
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/cheems/goindex-extended/css/mdui_v2.min.css">'
   );
 }
 if (UI.dark_mode) {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/cheems/goindex-extended/3456869088d565716450b227780432f4f717bb9e/css/nexmoe_v2_dark.min.css">'
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/cheems/goindex-extended/3456869088d565716450b227780432f4f717bb9e/css/nexmoe_v2_dark.min.css">'
   );
 } else {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/cheems/goindex-extended/3456869088d565716450b227780432f4f717bb9e/css/nexmoe_v2.min.css">'
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/cheems/goindex-extended/3456869088d565716450b227780432f4f717bb9e/css/nexmoe_v2.min.css">'
   );
 }
 document.write(
-  '<script src="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/mdui.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/cheems/goindex-extended/js/mdui.min.js"></script>'
 );
 document.write(
-  '<script src="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/cheems/goindex-extended/js/flv.min.js"></script>'
 );
 document.write(
-  '<script src="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/cheems/goindex-extended/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//rawcdn.githack.com/cheems/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/cheems/goindex-extended/js/markdown-it.min.js"></script>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
